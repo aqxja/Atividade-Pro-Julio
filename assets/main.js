c99b4af79d203fbdd0371331c93ef57adbb9017a
aqxja <aquiles123aq@gmail.com>
2023-08-11 07:27:51 -0300

    projeto retorno

--- assets/main.js
+++ assets/main.js
@@ -3,11 +3,3 @@ function mudar()
     console.log( "A" );
     document.body.style.backgroundImage = "url(assets/supernatural_wallpaper_by_breneige_da1znmp-fullview.jpg)";
 }
-function mudar()
-{
-    var btn = document.querySelector("#refresh");
-    btn.addEventListener("click", function() {
-    
-    location.reload();
-    })
-}
